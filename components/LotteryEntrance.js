fdadb6ac6e26a9981546dad17369a7a20b291b79
Yada dev <yadadev.io@gmail.com>
2022-07-06 21:18:47 -0400

    - connect backend to frontend - add tailwind

---
+++ components/LotteryEntrance.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LotteryEntrance = () => {
+    return (
+        <div>
+            <h1>Hi from lottery</h1>
+        </div>
+    );
+};
+
+export default LotteryEntrance;
